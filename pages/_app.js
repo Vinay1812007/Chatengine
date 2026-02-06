fb64c0efc85520649c1fd5ede092e77d6b871891
Vinay1812007 <131197559+Vinay1812007@users.noreply.github.com>
2026-02-06 14:11:15 +0530

    _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,5 @@
-// pages/_app.js
 import "../styles/globals.css";
-import { ThemeProvider } from "../lib/themeContext";
 
 export default function App({ Component, pageProps }) {
-  return (
-    <ThemeProvider>
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
+  return <Component {...pageProps} />;
 }
